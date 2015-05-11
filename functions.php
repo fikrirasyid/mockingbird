f7ac2e9f51a0f3929b128e0fe0a95c443b6057ff
Fikri Rasyid <fikrirasyid@gmail.com>
2015-05-11 11:08:12 +0700

    Adding theme support

--- functions.php
+++ functions.php
@@ -1,6 +1,28 @@
 <?php
 
 /**
+ * Theme setup
+ * 
+ * @return void
+ */
+function mockingbird_setup(){
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	// Let WordPress manage the document title.
+	add_theme_support( 'title-tag' );
+
+	// Enable support for Post Thumbnails on posts and pages.
+	add_theme_support( 'post-thumbnails' );	
+
+	// Switch default core markup for search form, comment form, and comments to output valid HTML5.
+	add_theme_support( 'html5', array(
+		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
+	) );	
+}
+add_action( 'after_setup_theme', 'mockingbird_setup' );
+
+/**
  * Load scripts
  * 
  * @return void
