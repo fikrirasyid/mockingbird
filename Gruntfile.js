4315848dac82331b1e2992eb98cbf4dccf3f2147
Fikri Rasyid <fikrirasyid@gmail.com>
2015-05-11 09:10:13 +0700

    Setup file, grunt, etc

---
+++ Gruntfile.js
@@ -0,0 +1,86 @@
+'use strict';
+module.exports = function(grunt){
+
+    // load all tasks
+    require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    "style.css" : "less/style.less"
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['less/**/*.less'],
+                tasks: ['less'],
+                options: {
+                    nospawn: true
+                }
+            }
+        },
+        makepot: {
+            target: {
+                options: {
+                    domainPath: '/languages/',    // Where to save the POT file.
+                    potFilename: '<%= pkg.name %>.pot',   // Name of the POT file.
+                    type: 'wp-theme'  // Type of project (wp-plugin or wp-theme).
+                }
+            }
+        },
+        clean: {
+            init: {
+                src: ['build/']
+            },
+            build: {
+                src: ['build/*', '!build/<%= pkg.name %>.zip']
+            }
+        },
+        copy: {
+            readme: {
+                src: 'readme.md',
+                dest: 'build/readme.txt'
+            },
+            build: {
+                expand: true,
+                src: ['**', '!node_modules/**', '!build/**', '!readme.md', '!Gruntfile.js', '!package.json' ],
+                dest: 'build/'
+            }
+        },
+        compress: {
+            build: {
+                options: {
+                    archive: 'build/<%= pkg.name %>.zip'
+                },
+                expand: true,
+                cwd: 'build/',
+                src: ['**/*'],
+                dest: '<%= pkg.name %>/'
+            }
+        }
+    });
+
+    grunt.registerTask('default', [ 'less', 'watch' ]);
+
+    // Build .pot file
+    grunt.registerTask( 'buildpot', [
+        'makepot'
+    ]);
+
+    // Build task
+    grunt.registerTask( 'build', [ 
+        'makepot',
+        'clean:init', 
+        'copy',
+        'compress:build',
+        'clean:build'
+    ]);   
+};
---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class();?>>
+		<div id="app">
+			<header id="header">
+				<button id="toggle-menu">Toggle Menu</button>
+				<h1 id="site-title">
+					<a href="<?php echo esc_url( site_url() ); ?>">
+						<?php bloginfo( 'name' ); ?>
+					</a>
+				</h1>
+			</header><!-- header#header -->
+			<div id="content">
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<div class="entry-meta">
+							<div class="entry-avatar">
+								<?php echo get_avatar( get_the_author_meta( 'user_email' ), 40 ); ?>
+							</div>
+							<h3 class="entry-author"><?php the_author(); ?></h3>
+							<p class="entry-date"><?php the_date(); ?></p>
+						</div>
+						<div class="entry-content">
+							<?php the_excerpt(); ?>
+						</div>	
+					</article>
+
+				<?php endwhile; ?>
+
+			<?php endif; ?>
+
+			</div><!-- #content -->
+		</div><!-- #app -->
+		<?php wp_footer(); ?>
+	</body>
+</html>
